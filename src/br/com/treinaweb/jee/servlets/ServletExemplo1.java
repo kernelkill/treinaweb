5d83aa3f9bea47c933fb118cf7140c5351a9d8c5
Joabe Kachorroski <kernelkill@outlook.com>
2016-02-11 23:08:52 -0300

    Sistema em Andamento da treinaweb

---
+++ src/br/com/treinaweb/jee/servlets/ServletExemplo1.java
@@ -0,0 +1,54 @@
+package br.com.treinaweb.jee.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletExemplo1
+ */
+@WebServlet(name = "ServletExemplo1", urlPatterns = ("/SvtEx1"), initParams = {
+		@WebInitParam(name = "mensagem", value = "Bem-Vindo ao teste de Servlets") }
+
+)
+public class ServletExemplo1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private String mensagem;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletExemplo1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+
+		mensagem = config.getInitParameter(mensagem);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nome = request.getParameter("nome");
+
+		response.getWriter().write("<h1> Teste de Servlet </h1>");
+		response.getWriter().write("<p> Bem Vindo " + nome + "</p>");
+	}
+
+}
